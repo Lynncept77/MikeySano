de9f9081f61b0bcba80ff2516e422db0427f65e5
Nor_baka <98837331+Baka-me@users.noreply.github.com>
2022-02-02 19:27:52 +0530

    Update __main__.py

--- MikeyBot/__main__.py
+++ MikeyBot/__main__.py
@@ -59,7 +59,7 @@ PM_START_TEXT = """
 * I am Mikey! An Anime Themed Super Duper Cool Advance Bot For Managing Your Groups .*
 ×~×~×~×~×~×~×~×~×~×~×~×
 • *Uptime:* `{}`
-• `{}` *users, across* `{}` *chats.*
+• `{}` *users, My fans 💥💥
 ×~×~×~×~×~×~×~×~×~×~×~×
 ➛ The Help Buttons That Are Below To Know How cool I am [.](pic?????) ××
 """
