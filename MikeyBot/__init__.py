a86a096e278a17cf1a51b8d84bbc23905c32b4a2
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-02-02 14:21:14 +0530

    Create __init__.py

---
+++ MikeyBot/__init__.py
@@ -0,0 +1,151 @@
+import logging
+import os
+import sys
+import time
+
+import telegram.ext as tg
+from pyrogram import Client, errors
+from redis import StrictRedis
+from telethon import TelegramClient
+from telethon.sessions import MemorySession
+
+StartTime = time.time()
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler(),
+    ],
+    level=logging.INFO,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+LOGGER.info("[MikeyBot] Starting Zeldris...")
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error(
+        "[MikeyBot] You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    sys.exit(1)
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("[MikeyBot] Your OWNER_ID env variable is not a valid integer.")
+
+    MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP", None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
+    except ValueError:
+        raise Exception(
+            "[MikeyBot] Your dev users list does not contain valid integers."
+        )
+       
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    URL = os.environ.get("URL", "")  # Does not contain token
+    PORT = int(os.environ.get("PORT", 5000))
+    CERT_PATH = os.environ.get("CERT_PATH")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    DB_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    REDIS_URL = os.environ.get("REDIS_URL")
+    DONATION_LINK = os.environ.get("DONATION_LINK")
+    LOAD = os.environ.get("LOAD", "").split()
+    TEMP_DOWNLOAD_DIRECTORY = ("./")
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    WORKERS = int(os.environ.get("WORKERS", 8))
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    CUSTOM_CMD = os.environ.get("CUSTOM_CMD", False)
+    API_WEATHER = os.environ.get("API_OPENWEATHER", None)
+    WALL_API = os.environ.get("WALL_API", None)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
+    API_ID = int(os.environ.get("API_ID", None))
+    API_HASH = os.environ.get("API_HASH", None)
+    
+else:
+    from MikeyBot.config import Development as Config
+
+    TOKEN = Config.TOKEN
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("[Zeldris] Your OWNER_ID variable is not a valid integer.")
+
+    MESSAGE_DUMP = Config.MESSAGE_DUMP
+    OWNER_USERNAME = Config.OWNER_USERNAME
+
+    try:
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
+    except ValueError:
+        raise Exception(
+            "[MikeyBot] Your dev users list does not contain valid integers."
+        )
+   
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    CERT_PATH = Config.CERT_PATH
+
+    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    REDIS_URL = Config.REDIS_URL
+    DONATION_LINK = Config.DONATION_LINK
+    LOAD = Config.LOAD
+    NO_LOAD = Config.NO_LOAD
+    DEL_CMDS = Config.DEL_CMDS
+    STRICT_GBAN = Config.STRICT_GBAN
+    WORKERS = Config.WORKERS
+    BAN_STICKER = Config.BAN_STICKER
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    CUSTOM_CMD = Config.CUSTOM_CMD
+    API_WEATHER = Config.API_OPENWEATHER
+    WALL_API = Config.WALL_API
+    SUPPORT_CHAT = Config.SUPPORT_CHAT
+    API_HASH = Config.API_HASH
+    API_ID = Config.API_ID
+    SPAMWATCH = Config.SPAMWATCH_API
+    SPAMMERS = Config.SPAMMERS
+
+# Dont Remove This!!!
+DEV_USERS.add(OWNER_ID)
+
+
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+try:
+    REDIS.ping()
+    LOGGER.info("[MikeyBot] Your redis server is now alive!")
+except BaseException:
+    raise Exception("[MikeyBot] Your redis server is not alive, please check again.")
+finally:
+    REDIS.ping()
+    LOGGER.info("[MikeyBot] Your redis server is now alive!")
+
+# Telethon
+client = TelegramClient(MemorySession(), API_ID, API_HASH)
+updater = tg.Updater(
+    TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
+)
+dispatcher = updater.dispatcher
+
+pbot = Client("ErenPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+
+pbot.start()
+
+app = pbot.get_me()
+BOT_ID = app.id
+
+DEV_USERS = list(DEV_USERS)
+
