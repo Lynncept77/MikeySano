ab41257039f9ba82954dc58047d0f768401a5e10
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-02-03 14:32:41 +0530

    Update __init__.py

--- MikeyBot/__init__.py
+++ MikeyBot/__init__.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-LOGGER.info("[MikeyBot] Starting Zeldris...")
+LOGGER.info("[MikeyBot] Starting Mikey...")
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
