40659cfecd186b11576662c8eb1171825555e293
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-02-03 14:09:37 +0530

    Update __init__.py

--- MikeyBot/__init__.py
+++ MikeyBot/__init__.py
@@ -63,7 +63,6 @@ if ENV:
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
     WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CUSTOM_CMD = os.environ.get("CUSTOM_CMD", False)
     API_WEATHER = os.environ.get("API_OPENWEATHER", None)
